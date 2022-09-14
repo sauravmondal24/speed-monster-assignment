910e7e0de79164bc3f832d5a9700e4d22cd158ee
sauravmondal24 <sauravmondal321@gmail.com>
2022-09-15 03:08:06 +0600

    mistakes problem solved

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const typeController = (e) => {
 			newLetter === ' ' ? '▪' : newLetter
 		}</span>`;
 	} else {
+		errorCount = errorCount + 1;
 		display.innerHTML += `<span class="red">${
 			newLetter === ' ' ? '▪' : newLetter
 		}</span>`;
